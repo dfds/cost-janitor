b1484b8fe48613f26e82d9729e37ce83c46ef0c8
Emil H. Clausen <sequoiia@hummel.yt>
2021-01-11 14:21:35 +0100

    Added http server

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/aws/aws-sdk-go v1.36.23 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	golang.org/x/text v0.3.5 // indirect
 )
--- main.go
+++ main.go
@@ -5,20 +5,37 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/costexplorer"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	fmt.Println("Launching cost-janitor")
 
+	r := mux.NewRouter()
+	r.HandleFunc("/get-monthly-total-cost/{accountid}", GetMonthlyTotalCost)
+
+	println("HTTP server listening on :8080")
+	if err := http.ListenAndServe("127.0.0.1:8080", handlers.LoggingHandler(os.Stdout, handlers.CompressHandler(r))); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetMonthlyTotalCost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("eu-central-1"),
-//		LogLevel: aws.LogLevel(aws.LogDebug),
+		//		LogLevel: aws.LogLevel(aws.LogDebug),
 	})
 	if err != nil {
-		log.Println("NewSession")
-		log.Fatal(err)
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		log.Println(err)
+		return
 	}
 
 	ce := costexplorer.New(sess)
@@ -30,7 +47,7 @@ func main() {
 	filter := &costexplorer.Expression{}
 	filter.SetDimensions(&costexplorer.DimensionValues{
 		Key:          aws.String(costexplorer.DimensionLinkedAccount),
-		Values:       []*string{aws.String(os.Getenv("AWS_ACC_ID"))},
+		Values:       []*string{aws.String(vars["accountid"])},
 	})
 
 	resp, err := ce.GetCostAndUsage(&costexplorer.GetCostAndUsageInput{
@@ -40,9 +57,12 @@ func main() {
 		Filter: filter,
 	})
 	if err != nil {
-		log.Println("GetCostAndUsage")
-		log.Fatal(err)
+		w.WriteHeader(500)
+		w.Write([]byte(err.Error()))
+		log.Println(err)
+		return
 	}
 
-	fmt.Println(resp.String())
+	w.WriteHeader(200)
+	w.Write([]byte(resp.String()))
 }
