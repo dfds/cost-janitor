bb7d9620376f5156a9d70673d5820db1f2d7b9b2
Emil H. Clausen <sequoiia@hummel.yt>
2021-01-11 20:07:36 +0100

    Add auth middleware for http endpoints

--- main.go
+++ main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/costexplorer"
+	"github.com/coreos/go-oidc"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Launching cost-janitor")
 
+	provider, err := oidc.NewProvider(context.Background(), "https://login.microsoftonline.com/73a99466-ad05-4221-9f90-e7142aa2f6c1/v2.0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authMiddleware := authenticationMiddleware{
+		ClientID: "24420be9-46e5-4584-acd7-64850d2f2a03",
+		Provider: provider,
+	}
+
+
+
 	r := mux.NewRouter()
 	r.HandleFunc("/get-monthly-total-cost/{accountid}", GetMonthlyTotalCost)
 
 	println("HTTP server listening on :8080")
-	if err := http.ListenAndServe("127.0.0.1:8080", handlers.LoggingHandler(os.Stdout, handlers.CompressHandler(r))); err != nil {
+	if err := http.ListenAndServe("127.0.0.1:8080", handlers.LoggingHandler(os.Stdout, handlers.CompressHandler(authMiddleware.Middleware(r)))); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -65,4 +80,45 @@ func GetMonthlyTotalCost(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(200)
 	w.Write([]byte(resp.String()))
+}
+
+
+type authenticationMiddleware struct {
+	ClientID string
+	Provider *oidc.Provider
+}
+
+func (amw *authenticationMiddleware) Middleware(next http.Handler) http.Handler {
+	var verifier = amw.Provider.Verifier(&oidc.Config{ClientID: amw.ClientID})
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqToken := r.Header.Get("Authorization") //Authorization: Bearer a7ydfs87afasd8f990
+		fmt.Printf("%+v\n", reqToken)
+		splitToken := strings.Split(reqToken, "Bearer")
+		if len(splitToken) != 2 {
+			http.Error(w, "Token doesn't seem right", http.StatusUnauthorized)
+			return
+		}
+
+		reqToken = strings.TrimSpace(splitToken[1])
+
+		idToken, err := verifier.Verify(r.Context(), reqToken)
+		if err != nil {
+			http.Error(w, "Unable to verify token", http.StatusUnauthorized)
+			return
+		}
+		fmt.Printf("%+v\n", idToken)
+
+		var claims struct {
+			Emails []string `json:"emails"`
+		}
+		if err := idToken.Claims(&claims); err != nil {
+			fmt.Println(err)
+			http.Error(w, "Unable to retrieve claims", http.StatusUnauthorized)
+			return
+		}
+		fmt.Printf("%+v\n", claims)
+
+		next.ServeHTTP(w, r)
+	})
 }
