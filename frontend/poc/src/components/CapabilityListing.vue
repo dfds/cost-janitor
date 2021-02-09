1131e8ee9c7263c68fcb73127f3d8c45d740cbc0
Emil H. Clausen <sequoiia@hummel.yt>
2021-02-09 22:28:26 +0100

    Initial Vue app creation

---
+++ frontend/poc/src/components/CapabilityListing.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="capabilityListing">
+      <ul>
+          <li>xaxa</li>
+          <li>xaxa</li>
+          <li>xaxa</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    font-size: 1.4em;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
+
+.capabilityListing {
+    margin-top: 10px;
+    margin-left: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+</style>
---
+++ frontend/poc/src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="inner">
+    <h1>Billing</h1>
+    <CapabilityListing />
+    </div>
+  </div>
+</template>
+
+<script>
+import CapabilityListing from './CapabilityListing.vue'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    CapabilityListing
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+h1 {
+  font-size: 4em;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+    width: 70%;
+    align-items: flex-start;
+    background-color: #423e48;
+    color: #c9c9c9;
+    height: 100vh;
+}
+
+.inner {
+  margin-left: 35px;
+  padding-top: 35px;
+}
+</style>
